678d187f29240faa37afa475db99f3e49c537cb3
Federico Blanc <fblanc92@gmail.com>
2021-02-13 17:44:22 -0300

    NON-JSX, Another way of creating elements: React.createElement

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Greeting() {
-  return <h1>Hello World!, this is the first component</h1>;
+// function Greeting() {
+//   return <h1>Hello World!, this is the first component</h1>;
+// }
+
+const Greeting = () => {
+    return React.createElement('h1', null, 'Hello world!');
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
