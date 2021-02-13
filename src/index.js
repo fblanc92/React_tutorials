93851abccfcb740f03565aefd67d960cb3715035
Federico Blanc <fblanc92@gmail.com>
2021-02-13 18:22:59 -0300

    Anidating elements with React.createElement

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 // function Greeting() {
-//   return <h1>Hello World!, this is the first component</h1>;
+//   return (<div>
+//             <h1>Hello World!, this is the first component</h1>
+//         </div>);
 // }
 
 const Greeting = () => {
-    return React.createElement('h1', null, 'Hello world!');
+    return React.createElement('div', null, React.createElement('h1', {}, <h1>Hello World!</h1>));
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
