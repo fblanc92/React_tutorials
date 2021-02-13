f953217a4153b4dbca21912e7a77fdd29342d05d
Federico Blanc <fblanc92@gmail.com>
2021-02-13 18:29:21 -0300

    Fragments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// function Greeting() {
-//   return (<div>
-//             <h1>Hello World!, this is the first component</h1>
-//         </div>);
-// }
-
-const Greeting = () => {
-    return React.createElement('div', null, React.createElement('h1', {}, <h1>Hello World!</h1>));
+function Greeting() {
+    return (
+        <>
+            <div>
+                <h3>Hello World!</h3>
+            </div>
+        </>
+    )
 }
+
+// const Greeting = () => {
+//     return React.createElement('div', null, React.createElement('h1', {}, <h1>Hello World!</h1>));
+// }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
